c092034f8b330e124ba28292cd6043a6394cfa75
Jakub Kozik <jakubkozik120@gmail.com>
2022-04-29 18:35:42 +0200

    Add addColumn function in store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,21 @@ import { createStore } from '@reduxjs/toolkit';
 import shortid from 'shortid';
 import initialState from './initialState';
 import { strContains } from '../utils/strContains';
+// import { createSelector } from '@reduxjs/toolkit';
 
 export const getFilteredCards = ({ cards, searchInput }, columnId) => cards.filter(card => card.columnId === columnId && strContains(card.title, searchInput));
+export const getAllColumns = state => state.columns;
 
+// action creators
+
+export const addColumn = payload => ({type: 'ADD_COLUMN', payload});
+
+
+/* Example of memo - to remove*/
+// const selectDupa = createSelector([getFilteredCards, getAllColumns], (cards, columns) => {
+//   return {cards, columns}
+// })
+// console.log(selectDupa());
 
 const reducer = (state, action) => {
   switch(action.type) {
